ad5f112db3a316694b0bdae143a8ec2674ffa065
Warren Yao <warrenyca905@gmail.com>
2019-05-22 02:54:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,11 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+require 'pry'
+
 def key_for_min_value(name_hash)
   return nil if name_hash == {}
+  
   collect = []
   temp = nil
   val = nil
@@ -10,6 +13,7 @@ def key_for_min_value(name_hash)
   name_hash.collect do |key, value|
     collect << value
   end
+  
   collect.each_with_index do |tests, index|
     if index == 0
       temp = tests
