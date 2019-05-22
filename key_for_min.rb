cbf99ff8f396a9536844fa1b481340e9e76e3c4e
Warren Yao <warrenyca905@gmail.com>
2019-05-22 03:09:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,14 +8,17 @@ def key_for_min_value(name_hash)
   
   count = 0
   temp = nil
+  least = nil
   
   name_hash.each do |key, value|
     if count == 0
-      temp = key
+      temp = value
+      least = key
     elsif value < temp
-      temp = key
+      temp = value
+      least = key
     end
     count += 1
   end
-  return name_hash[key]
+  return name_hash[least]
 end
