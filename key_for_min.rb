7a3388a43abe06b71f312c34f5e4728a9245dfd6
Warren Yao <warrenyca905@gmail.com>
2019-05-22 02:47:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  return nil if name_hash.empty? == true
+  return nil if name_hash == {}
   collect = []
   name_hash.collect do |key, value|
     collect << value
