2030e7bb83d67d288cc580026d1d31233ce8954c
Warren Yao <warrenyca905@gmail.com>
2019-05-22 03:07:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,21 +6,16 @@ require 'pry'
 def key_for_min_value(name_hash)
   return nil if name_hash == {}
   
-  collect = []
+  count = 0
   temp = nil
-  val = nil
   
-  name_hash.collect do |key, value|
-    collect << value
-  end
-  
-  collect.each_with_index do |tests, index|
-    if index == 0
-      temp = tests
-    elsif tests < temp
-      temp = tests
-      val = index
+  name_hash.each do |key, value|
+    if count == 0
+      temp = value
+    elsif value < temp
+      temp = value
     end
+    count += 1
   end
-  return collect[val]
+  return temp
 end
